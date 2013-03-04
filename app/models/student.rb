b60ca0aa6e0d3afeb0e6e1213090b0a0070b6696
David Mills <dave@1morepx.com>
2013-03-04 17:25:09 -0500

    added very simple seeds and fixed mass assign on student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,4 @@
 class Student < ActiveRecord::Base
   belongs_to :seat
-  attr_accessible :first_name, :last_name
+  attr_accessible :first_name, :last_name, :seat
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+seats = Seat.create([
+  { name: 'A' },
+  { name: 'B' },
+  { name: 'C' },
+  { name: 'D' }
+])
+
+Student.create([
+  {
+    first_name: 'John',
+    last_name: 'Doe',
+    seat: seats[0]
+  },
+  {
+    first_name: 'Jane',
+    last_name: 'Doe',
+    seat: seats[0]
+  }
+])
