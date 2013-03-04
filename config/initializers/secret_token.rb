720b4ecd69fc2dfa64aa64215bd850ab0add87fc
D1plo1d <d1plo1d@d1plo1d.com>
2013-03-04 14:08:40 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadHatterbot::Application.config.secret_token = 'd9b0745f26bb306c63e39b84c32fcd09b6a9089e8a5458ab1cd6edfd7a7339e61e1725456d5aa0c9d0a08bc7b5557531497bae4140f36e65c81cd5686e604209'
